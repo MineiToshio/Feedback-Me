80c85b6f6c6a90bdc1024fdfdfaba8608f0dba35
Toshio Minei <sminei10@gmail.com>
2024-02-17 02:14:12 -0500

    chore: set tailwind custom colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,52 @@
+const colors = {
+  primary: {
+    light: "#AD1FEA",
+    DEFAULT: "#AD1FEA",
+  },
+  "primary-hover": {
+    light: "#C75AF6",
+    DEFAULT: "#C75AF6",
+  },
+  secondary: {
+    light: "#4661E6",
+    DEFAULT: "#4661E6",
+  },
+  "secondary-hover": {
+    light: "#7C91F9",
+    DEFAULT: "#7C91F9",
+  },
+  error: {
+    light: "#E98888",
+    DEFAULT: "#E98888",
+  },
+  background: {
+    light: "#F2F4FF",
+    DEFAULT: "#F2F4FF",
+  },
+  background2: {
+    light: "#F2F4FE",
+    DEFAULT: "#F2F4FE",
+  },
+  form: {
+    light: "#F7F8FD",
+    DEFAULT: "#F7F8FD",
+  },
+  text: {
+    light: "#3A4374",
+    DEFAULT: "#3A4374",
+  },
+  header: {
+    light: "#373F68",
+    DEFAULT: "#373F68",
+  },
+  planned: {
+    light: "#F49F85",
+    DEFAULT: "#F49F85",
+  },
+  live: {
+    light: "#62BCFA",
+    DEFAULT: "#62BCFA",
+  },
+};
+
+export default colors;
