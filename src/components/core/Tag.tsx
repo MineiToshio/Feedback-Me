e7cd57c0de55539497af749e8a2d45f376d6acbf
Toshio Minei <sminei10@gmail.com>
2024-02-21 02:45:47 -0500

    chore: created Tag component

---
+++ src/components/core/Tag.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/styles/utils";
+import { FC } from "react";
+
+type TagProps = {
+  text: string;
+  active?: boolean;
+  className?: string;
+};
+
+const Tag: FC<TagProps> = ({ text, active, className }) => {
+  return (
+    <div
+      className={cn(
+        "h-[30px] w-min px-4 flex items-center justify-center font-semibold text-[13px] text-secondary bg-background2 hover:bg-background2-hover rounded cursor-pointer",
+        { "bg-secondary hover:bg-secondary text-white": active },
+        className,
+      )}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default Tag;
