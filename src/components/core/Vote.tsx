8f20d8eee6b890a7f914596a384da5319d51323e
Toshio Minei <sminei10@gmail.com>
2024-02-21 02:45:30 -0500

    chore: added className prop in Vote component

--- src/components/core/Vote.tsx
+++ src/components/core/Vote.tsx
@@ -5,14 +5,16 @@ import { cn } from "@/styles/utils";
 type VoteProps = {
   count: number;
   active?: boolean;
+  className?: string;
 };
 
-const Vote: FC<VoteProps> = ({ count, active }) => {
+const Vote: FC<VoteProps> = ({ count, active, className }) => {
   return (
     <div
       className={cn(
         "h-[53px] w-10 flex items-center justify-center flex-col font-bold text-[13px] text-text bg-background2 hover:bg-background2-hover rounded cursor-pointer",
         { "bg-secondary hover:bg-secondary text-white": active },
+        className,
       )}
     >
       <Icon.ArrowUpIcon
