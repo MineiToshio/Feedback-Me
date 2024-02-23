3009323f31aa6f07cd9c4994d7e24c72fdf7d6d8
Toshio Minei <sminei10@gmail.com>
2024-02-23 01:55:11 -0500

    chore: setup jest

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  preset: "ts-jest",
+  coverageProvider: "v8",
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleNameMapper: {
+    "^@/src/(.*)$": "<rootDir>/src/$1",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ["src/**/*.ts", "!src/styles/*"],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
+
+export default createJestConfig(config);
